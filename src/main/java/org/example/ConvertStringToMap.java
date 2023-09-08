5b35edb7acdd795459b26d80c542636773711ebb
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-09-08 20:52:21 +0700

    Dell sout

--- src/main/java/org/example/ConvertStringToMap.java
+++ src/main/java/org/example/ConvertStringToMap.java
@@ -4,7 +4,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ConvertStringToMap {
@@ -13,8 +12,7 @@ public class ConvertStringToMap {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             map = objectMapper.readValue(json, HashMap.class);
-            System.out.println(map.values());
-            LinkedHashMap m = (LinkedHashMap) map.get("book");
+            HashMap m = (HashMap) map.get("book");
             System.out.println(m.get("id"));
         } catch (IOException e) {
             throw new RuntimeException(e);
